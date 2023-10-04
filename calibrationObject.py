16e8f4b1d1642ad91e663bccf459e56d5a77580f
Hannah-Puhov <105806067+Hannah-Puhov@users.noreply.github.com>
2023-10-04 16:06:43 -0400

    Created calibration object class, new empty python files for other data i/o

---
+++ calibrationObject.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import Point
+
+class CalibrationObject:
+
+    numBaseMarkers = 0
+    numOptCalMarkers = 0
+    numEMCalMarkers = 0
+    d = []
+    a = []
+    c = []
+
+    def __init__(self, folder, name):
+        self.fileExtension = '-calbody.txt'
+        self.fileName =  folder + '/' + name + self.fileExtension
+        self.data = pd.read_csv(self.fileName, delimiter=',')
+        self.data_setup()
+
+    def data_setup(self):
+
+        x_coors = self.data.iloc[:,0]
+        y_coors = self.data.iloc[:,1]
+        z_coors = self.data.iloc[:,2]
+
+        numBaseMarkers = int(self.data.columns[0])
+        numOptCalMarkers = int(self.data.columns[1])
+        numEMCalMarkers = int(self.data.columns[2])
+
+        for i in range(numBaseMarkers + numOptCalMarkers + numEMCalMarkers):
+            p = Point(x_coors[i], y_coors[i], z_coors[i])
+            if i < numBaseMarkers:
+                self.d.append(p)
+            elif i < numBaseMarkers + numOptCalMarkers:
+                self.a.append(p)
+            else:
+                self.c.append(p)
+
+
+
+
+        
+
+calObj = CalibrationObject('PA1 Student Data', 'pa1-unknown-j')
+
+
+
