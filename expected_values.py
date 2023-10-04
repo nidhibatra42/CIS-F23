72092ef31bf0cbc4e9b0d4d1d4b753b3ba0fca50
nidhibatra42 <98132690+nidhibatra42@users.noreply.github.com>
2023-10-04 15:28:23 -0400

    question 4 - still need to create output file class

---
+++ expected_values.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def expected_values(Dj, Aj, Cj):
+    #compute transformation between optical tracker and EM coordinates 
+
+    #Dj
+    #Find Do
+    Do = np.mean(Dj)
+    #find dj
+    dj = Dj - Do
+    #Aj
+    #Find Ao
+    Ao = np.mean(Aj)
+    #find aj
+    aj = Aj - Ao
+    #Cj
+    #Find Co
+    Co = np.mean(Cj)
+    #find cj
+    cj = Cj - Co
+
+    #compute transformation FD
+    FD = np.dot(Dj, np.linalg.inv(dj))
+
+    #compute transformation FA
+    FA = np.dot(Aj, np.linalg.inv(aj))
+
+    #compute Ci expected 
+    FDFA = np.dot(np.linalg.inv(FD), FA)
+    CiExpected = np.dot(FDFA, cj)
+
+    #output Ci expected 
+    OutputCreator(CiExpected)
+   
