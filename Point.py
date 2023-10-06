d7025a173541e2b4d78be9d7d4f3c1f0044207c3
Hannah-Puhov <105806067+Hannah-Puhov@users.noreply.github.com>
2023-10-06 14:21:01 -0400

    added from_array() method to point class

--- Point.py
+++ Point.py
@@ -1,11 +1,16 @@
 class Point:
-    def _init_(self, x, y, z):
+    def __init__(self, x = 0, y = 0, z = 0):
         self.x = x
         self.y = y
         self.z = z
     
     def to_array(self):
         return [self.x, self.y, self.z]
+    
+    def from_array(self, pointArray):
+        self.x = pointArray[0]
+        self.y = pointArray[1]
+        self.z = pointArray[2]
 
     
 
