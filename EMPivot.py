3cb9e9ede31f57b51265a7d51640ac57e7754a11
nidhibatra42 <98132690+nidhibatra42@users.noreply.github.com>
2023-10-11 19:28:59 -0400

    added comments

--- EMPivot.py
+++ EMPivot.py
@@ -3,26 +3,24 @@ import pandas as pd
 import numpy as np
 
 class EMPivot:
-    """_summary_
-    """
+    """Class for processing EMPivot data."""
     numFrames = 0
     g = [[]]
 
     def __init__(self, folder, name):
-        """_summary_
+        """Initialize EMPivot object.
 
         Args:
-            folder (_type_): _description_
-            name (_type_): _description_
-        """        
+            folder (str): The folder path where EMPivot data is located.
+            name (str): The name of the EMPivot data file.
+        """      
         self.fileExtension = '-empivot.txt'
         self.fileName =  folder + '/' + name + self.fileExtension
         self.data = pd.read_csv(self.fileName, delimiter=',')
         self.data_setup()
 
     def data_setup(self):
-        """_summary_
-        """        
+        """Setup data from the text file."""        
         x_coors = self.data.iloc[:,0]
         y_coors = self.data.iloc[:,1]
         z_coors = self.data.iloc[:,2]
@@ -41,8 +39,7 @@ class EMPivot:
         self.array_setup()
 
     def array_setup(self):
-        """_summary_
-        """        
+        """Setup an array for EMPivot data."""    
         self.GArray = []
 
         for i in range(self.numFrames):
