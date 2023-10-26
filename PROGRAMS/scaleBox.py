40ea7e05efa2878a05fc57d6bbc09f74acbc10b9
Hannah-Puhov <105806067+Hannah-Puhov@users.noreply.github.com>
2023-10-25 21:31:22 -0400

    Finished distortion correction ?

--- PROGRAMS/scaleBox.py
+++ PROGRAMS/scaleBox.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from math import comb
 from expectedValues import expected_values
 from calReadings import CalReadings
 from calBody import CalBody
@@ -7,20 +8,22 @@ from calBody import CalBody
 
 class BoxScale:
     
+    degree = 5
+
     def __init__(self, fileName, inputFolder):
-        calRead = CalReadings(inputFolder, fileName)
-        calObj = CalBody(inputFolder, fileName)
+        self.calRead = CalReadings(inputFolder, fileName)
   
         #find actual from expected using function from assignment 1, 4 
-        self.ci = []
-        for i in range(calRead.numFrames):
-            self.ci.add_frame(expected_values(calRead.dArray[i], calRead.aArray[i], calObj.dArray, calObj.aArray, calObj.cArray))
-    
+        self.ciExpected = []
+        for i in range(self.calRead.numFrames):
+            self.ciExpected.add_frame(expected_values(self.calRead.dArray[i], self.calRead.aArray[i], self.calObj.dArray, self.calObj.aArray, self.calObj.cArray))
+
+        self.ci = self.calRead.cArray
 
 
 
     #need to determine bounding box to scale values
-    def createScaleBox(self):
+    def create_scale_box(self):
         #Max and min store the maxes and mins of each data frame as [x_max_i, y_max_i, z_max_i]
         self.maxes = []
         self.mins = []
@@ -36,21 +39,52 @@ class BoxScale:
             min.append(ci_df.z.max)
             self.maxes.append(max)
             self.mins.append(min)
-            
-
-        
-        self.ci_max_x = self.ci_df.x.max
-        self.ci_min_y = self.ci.x.min()
-        return (ci_current - self.ci_min_axis)/ (self.ci_max_axis - self.ci_min_axis)
     
-
-    #This is running our distortion correction
-    def distortionCorrection(self):
-        String x
-        us = []
-        for ci_current in self.ci: 
-            us.append = self.scaleBox(ci_current)
+    def scale_to_box(self, point, frame):
+        scaledPoint = []
+        for i in range(3):
+            scaledPoint.append((point[i] - self.mins[frame][i]) / self.maxes[frame][i] - self.mins[frame][i])
         
-        #find polynomials using least squares
+        return scaledPoint
+            
+    
+    def single_coor_bernstein(self, coor, i):
+        return comb(self.degree, i) * pow(coor, i) * pow((1 - coor), (self.degree - i))
+
+
+    #Distortion correction for points -> list of the points in a given frame
+    def create_F(self, points, k):
+        F = []
+
+        scaledPoints = []
+        for point in points:
+            scaledPoints.append(self.scale_to_box(point, k))
+
+        for point in scaledPoints:
+            F.append(self.create_f_row(point))
+
+    def create_f_row(self, point):
+        f_row = []
+        for i in range(self.maxDegree + 1):
+            for j in range(self.maxDegree + 1):
+                for k in range(self.maxDegree + 1):
+                    bs = []
+                    bs.append(self.single_coor_bernstein(point[0], i))
+                    bs.append(self.single_coor_bernstein(point[1], j))
+                    bs.append(self.single_coor_bernstein(point[2], k))
+
+                    f_row.append(bs[0] * bs[1] * bs[2])
+
+
+    def distortion_correction(self):
+        correctedPoints = []
+        for k in range(self.calRead.numFrames):
+            F = self.create_F(self.ci[k], k)
+
+            calMatrix = np.linalg.lstsq(F, self.ciExpected[k], None)
+            correctedPoints.append(np.dot(F, calMatrix))
+        
+        return correctedPoints
+            
 
 
