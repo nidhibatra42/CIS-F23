f90a7d05a5585daca334580ed48d9ec408a75eb4
Nidhi Batra <Nidhi@MacBook-Pro-4.local>
2023-11-01 21:51:22 -0400

    renaming main2 functions being called

--- PROGRAMS/main2.py
+++ PROGRAMS/main2.py
@@ -1,4 +1,4 @@
-from scaleBox import BoxScale
+from FindingTipCT import FindingTipCT
 import os
 
 currentFolder = os.getcwd()
@@ -10,7 +10,7 @@ outputFolder = "OUTPUT"
 inputFolder = os.path.join(parentFolder, inputFolder)
 outputFolder = os.path.join(parentFolder, outputFolder)
 
-prog = BoxScale('pa2-debug-a', inputFolder, outputFolder)
+prog = FindingTipCT('pa2-debug-a', inputFolder, outputFolder)
 
 prog.create_scale_box()
 prog.get_ci_expecteds()
