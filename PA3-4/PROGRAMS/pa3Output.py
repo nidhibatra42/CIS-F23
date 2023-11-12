666e3f33fcc00ae7859533c8b8eb662beb7460f5
Hannah-Puhov <105806067+Hannah-Puhov@users.noreply.github.com>
2023-11-12 14:39:26 -0500

    Created output file

---
+++ PA3-4/PROGRAMS/pa3Output.py
@@ -0,0 +1,41 @@
+import os
+
+class pa3Output:
+    """Class for writing output data to a text file."""
+    def __init__(self, folder, name, numFrames, problem):
+        """Initialize the OutputWriter.
+
+        Args:
+            folder (str): The folder path where the output file will be saved.
+            name (str): The name of the output file.
+            numFrames (int): The total number of frames in the output.
+            problem (int): problem 3 or 4
+        """       
+        self.fileExtension = 'pa3-' + str(problem) + '-Output.txt'
+        self.fileName =  folder + '/' + name + self.fileExtension
+
+        if not os.path.exists(folder):
+             os.makedirs(folder)
+
+        with open(self.fileName, 'w') as output:
+            line1 = f"{numFrames}, {name + self.fileExtension}\n"
+            output.write(line1)
+            output.close()
+        
+
+    def add_record(self, pivot):
+        """Add the coordinates of a pivot to the output file
+
+        Args:
+            pivot (1x7 array): coordinates of dk, ck in 3D space, 
+            difference magnitude
+        """   
+
+        with open (self.fileName, 'a') as output:
+                rounded_pivot = [round(x, 2) for x in pivot]
+                output.write(', '.join(map(str, rounded_pivot)) + '\n')
+        output.close()  
+
+
+
+
